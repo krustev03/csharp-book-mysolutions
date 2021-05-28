7472ee975e5b59e2be333e390538c959500ea418
Golbarg200 <pepi_krastev@abv.bg>
2021-05-28 13:16:29 +0300

    added / trees and graphs / ex.14, 15, 16 solution

---
+++ ChapterSeventeen-TreesAndGraphs-Exercise/15.MinPaths/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15.MinPaths
+{
+    class Program
+    {
+        public class Edge
+        {
+            private int from;
+            private int to;
+            private int weight;
+
+            public Edge(int f, int t, int w)
+            {
+                from = f;
+                to = t;
+                weight = w;
+            }
+
+            public int From
+            {
+                get => this.from;
+                set => this.from = value;
+            }
+
+            public int To
+            {
+                get => this.to;
+                set => this.to = value;
+            }
+
+            public int Weight
+            {
+                get => this.weight;
+                set => this.weight = value;
+            }
+        }
+
+        static Dictionary<int, List<Edge>> graph = new Dictionary<int, List<Edge>>();
+
+        static void Main(string[] args)
+        {
+            int n = 7;
+            for (int i = 0; i < n; i++)
+            {
+                graph.Add(i, new List<Edge>());
+            }
+
+            graph[0].Add(new Edge(0, 1, 3));
+            graph[0].Add(new Edge(0, 2, 2));
+            graph[0].Add(new Edge(0, 5, 3));
+            graph[1].Add(new Edge(1, 3, 1));
+            graph[1].Add(new Edge(1, 2, 6));
+            graph[2].Add(new Edge(2, 3, 1));
+            graph[2].Add(new Edge(2, 4, 10));
+            graph[3].Add(new Edge(3, 4, 5));
+            graph[5].Add(new Edge(5, 4, 7));
+
+            var result = Djikstra(0);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == int.MaxValue)
+                {
+                    Console.WriteLine(i + "-no path");
+                }
+                else
+                {
+                    Console.WriteLine(i + "-" + result[i]);
+                }
+            }
+        }
+
+        static int[] Djikstra(int s)
+        {
+            int[] dist = new int[graph.Count];
+            bool[] visited = new bool[graph.Count];
+
+            for (int i = 0; i < dist.Length; i++)
+            {
+                dist[i] = int.MaxValue;
+            }
+
+            dist[s] = 0;
+
+            var distDic = new SortedDictionary<int, int>();
+
+            distDic.Add(s, 0);
+
+            while (distDic.Any())
+            {
+                var element = distDic.ElementAt(0);
+                distDic.Remove(element.Key);
+
+                var index = element.Key;
+
+                visited[index] = true;
+
+                foreach (var edge in graph[index])
+                {
+                    if (visited[edge.To])
+                    {
+                        continue;
+                    }
+
+                    var newDist = dist[index] + edge.Weight;
+
+                    if (newDist < dist[edge.To])
+                    {
+                        dist[edge.To] = newDist;
+
+                        if (!distDic.ContainsKey(edge.To))
+                        {
+                            distDic.Add(edge.To, newDist);
+                        }
+                        else
+                        {
+                            distDic[edge.To] = newDist;
+                        }
+                    }
+                }
+            }
+
+            return dist;
+        }
+    }
+}
---
+++ ChapterSeventeen-TreesAndGraphs-Exercise/16.NTasks/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _16.NTasks
+{
+    class Program
+    {
+        public class Edge
+        {
+            private int from;
+            private int to;
+            private int weight;
+
+            public Edge(int f, int t, int w)
+            {
+                from = f;
+                to = t;
+                weight = w;
+            }
+
+            public int From
+            {
+                get => this.from;
+                set => this.from = value;
+            }
+
+            public int To
+            {
+                get => this.to;
+                set => this.to = value;
+            }
+
+            public int Weight
+            {
+                get => this.weight;
+                set => this.weight = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 6;
+            Dictionary<int, List<Edge>> graph = new Dictionary<int, List<Edge>>();
+            for (int i = 0; i < n; i++) graph.Add(i, new List<Edge>());
+            graph[1].Add(new Edge(1, 2, 3));
+            graph[2].Add(new Edge(2, 5, 2));
+            graph[2].Add(new Edge(2, 4, 3));
+            graph[3].Add(new Edge(3, 1, 1));
+
+            int[] ordering = TopologicalSort(graph, n);
+
+            Console.WriteLine(String.Join(' ', ordering));
+        }
+
+        private static int DFS(int i, int at, bool[] visited, int[] ordering, Dictionary<int, List<Edge>> graph)
+        {
+
+            visited[at] = true;
+
+            List<Edge> edges = graph[at];
+
+            if (edges != null)
+            {
+                foreach (Edge edge in edges)
+                {
+                    if (!visited[edge.To])
+                    {
+                        i = DFS(i, edge.To, visited, ordering, graph);
+                    }
+                }
+            }
+
+            ordering[i] = at;
+            return i - 1;
+        }
+
+        public static int[] TopologicalSort(Dictionary<int, List<Edge>> graph, int numNodes)
+        {
+            int[] ordering = new int[numNodes];
+            bool[] visited = new bool[numNodes];
+
+            int i = numNodes - 1;
+            for (int at = 0; at < numNodes; at++)
+            {
+                if (!visited[at])
+                {
+                    i = DFS(i, at, visited, ordering, graph);
+                }
+            }
+
+            return ordering;
+        }
+    }
+}
