49cfcb89f09e2535a47c02f1b61ae9e0beb106ec
Golbarg200 <pepi_krastev@abv.bg>
2021-05-31 18:30:42 +0300

    added / data structures (new) / ex.2 solution

---
+++ ChapterSixteen-DataStructures-Exercise/02.PrintInReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.PrintInReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Console.Write("Count of nums: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                stack.Push(num);
+            }
+
+            Console.WriteLine("Nums in reverse order: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
