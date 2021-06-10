f856209eab3aee13ff5a8b489c91eb65dcbdd44e
Golbarg200 <pepi_krastev@abv.bg>
2021-06-10 09:49:46 +0300

    added / how to solve / ex 3, 4 solutions

--- ChapterTwentyThree-HowToSolve-Exercise/03.StreetMap/Program.cs
+++ ChapterTwentyThree-HowToSolve-Exercise/03.StreetMap/Program.cs
@@ -1,44 +1,180 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.StreetMap
 {
     class Program
     {
-        public class Edge
+        public class CrossRoad
         {
-            private string crOne;
-            private string crTwo;
-            private int length;
+            private string name;
+            private int index;
+            private Dictionary<CrossRoad, int> streets;
 
-            public Edge(string crOne, string crTwo, int l)
+            public CrossRoad(string name, int index)
             {
-                this.crOne = crOne;
-                this.crTwo = crTwo;
-                this.length = l;
+                this.name = name;
+                this.index = index;
+                this.streets = new Dictionary<CrossRoad, int>();
             }
 
-            public string CrOne
+            public string Name
             {
-                get => this.crOne;
-                set => this.crOne = value;
+                get => this.name;
+                set => this.name = value;
             }
 
-            public string CrTwo
+            public int Index
             {
-                get => this.crTwo;
-                set => this.crTwo = value;
+                get => this.index;
+                set => this.index = value;
             }
 
-            public int Length
+            public Dictionary<CrossRoad, int> Streets
             {
-                get => this.length;
-                set => this.length = value;
+                get => this.streets;
+                set => this.streets = value;
             }
         }
 
+        static List<CrossRoad> map = new List<CrossRoad>();
+        static List<string[]> pathsToFind = new List<string[]>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProceedInput();
+
+            foreach (var path in pathsToFind)
+            {
+                var startIndex = map.Where(cr => cr.Name == path[0]).FirstOrDefault().Index;
+                var endIndex = map.Where(cr => cr.Name == path[1]).FirstOrDefault().Index;
+
+                var result = Djikstra(startIndex);
+
+                if (result[endIndex] == int.MaxValue)
+                {
+                    Console.WriteLine("No path!");
+                    continue;
+                }
+                Console.WriteLine(result[endIndex]);
+            }
+        }
+
+        static int[] Djikstra(int s)
+        {
+            int[] dist = new int[map.Count];
+            bool[] visited = new bool[map.Count];
+
+            for (int i = 0; i < dist.Length; i++)
+            {
+                dist[i] = int.MaxValue;
+            }
+
+            dist[s] = 0;
+
+            var distDic = new SortedDictionary<int, int>();
+
+            distDic.Add(s, 0);
+
+            while (distDic.Any())
+            {
+                var element = distDic.ElementAt(0);
+                distDic.Remove(element.Key);
+
+                var index = element.Key;
+                var minValue = element.Value;
+
+                visited[index] = true;
+
+                if (dist[index] < minValue)
+                {
+                    continue;
+                }
+
+                foreach (var edge in map.Where(cr => cr.Index == index).FirstOrDefault().Streets)
+                {
+                    if (visited[edge.Key.Index])
+                    {
+                        continue;
+                    }
+
+                    var newDist = dist[index] + edge.Value;
+
+                    if (newDist < dist[edge.Key.Index])
+                    {
+                        dist[edge.Key.Index] = newDist;
+
+                        if (!distDic.ContainsKey(edge.Key.Index))
+                        {
+                            distDic.Add(edge.Key.Index, newDist);
+                        }
+                        else
+                        {
+                            distDic[edge.Key.Index] = newDist;
+                        }
+                    }
+                }
+            }
+
+            return dist;
+        }
+
+        static void ProceedInput()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"map.txt");
+            string[] files = File.ReadAllLines(path);
+
+            int counter = 0;
+            int index = 0;
+            bool proceededStreets = false;
+
+            foreach (var line in files)
+            {
+                if (line == "")
+                {
+                    if (proceededStreets)
+                    {
+                        break;
+                    }
+
+                    proceededStreets = true;
+                    continue;
+                }
+
+                string[] tokens = line.Split(' ');
+
+                if (!proceededStreets)
+                {
+                    var firstCr = map.Where(cr => cr.Name == tokens[0]).FirstOrDefault();
+
+                    if (firstCr == null)
+                    {
+                        firstCr = new CrossRoad(tokens[0], index);
+                        index++;
+                        map.Add(firstCr);
+                    }
+
+                    var secondCr = map.Where(cr => cr.Name == tokens[1]).FirstOrDefault();
+
+                    if (secondCr == null)
+                    {
+                        secondCr = new CrossRoad(tokens[1], index);
+                        index++;
+                        map.Add(secondCr);
+                    }
+
+                    firstCr.Streets.Add(secondCr, int.Parse(tokens[2]));
+                    secondCr.Streets.Add(firstCr, int.Parse(tokens[2]));
+                    counter++;
+                }
+                else
+                {
+                    pathsToFind.Add(new string[2] { tokens[0], tokens[1] });
+                }
+            }
         }
     }
 }
---
+++ ChapterTwentyThree-HowToSolve-Exercise/04.ConvexHull/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ConvexHull
+{
+    class Point : IComparable<Point>
+    {
+        private int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public int CompareTo(Point other)
+        {
+            return x.CompareTo(other.x);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", x, y);
+        }
+    }
+
+    class Program
+    {
+        private static List<Point> ConvexHull(List<Point> p)
+        {
+            if (p.Count == 0) return new List<Point>();
+            p.Sort();
+            List<Point> h = new List<Point>();
+
+            // lower hull
+            foreach (var pt in p)
+            {
+                while (h.Count >= 2 && !Ccw(h[h.Count - 2], h[h.Count - 1], pt))
+                {
+                    h.RemoveAt(h.Count - 1);
+                }
+                h.Add(pt);
+            }
+
+            // upper hull
+            int t = h.Count + 1;
+            for (int i = p.Count - 1; i >= 0; i--)
+            {
+                Point pt = p[i];
+                while (h.Count >= t && !Ccw(h[h.Count - 2], h[h.Count - 1], pt))
+                {
+                    h.RemoveAt(h.Count - 1);
+                }
+                h.Add(pt);
+            }
+
+            h.RemoveAt(h.Count - 1);
+            return h;
+        }
+
+        private static bool Ccw(Point a, Point b, Point c)
+        {
+            return ((b.X - a.X) * (c.Y - a.Y)) > ((b.Y - a.Y) * (c.X - a.X));
+        }
+
+        static void Main(string[] args)
+        {
+            List<Point> points = new List<Point>() {
+                new Point(10, 10),
+                new Point(20, 20),
+                new Point(50, 20),
+                new Point(80, 20),
+                new Point(100, 30),
+
+                new Point(20, 70),
+                new Point(30, 70),
+                new Point(30, 60),
+                new Point(40, 40),
+                new Point(60, 50),
+
+                new Point(70, 80),
+                new Point(90, 60),
+                new Point(110, 70)
+            };
+
+            List<Point> hull = ConvexHull(points);
+            Console.Write("Convex Hull: [");
+            for (int i = 0; i < hull.Count; i++)
+            {
+                if (i > 0)
+                {
+                    Console.Write(", ");
+                }
+                Point pt = hull[i];
+                Console.Write(pt);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
